8dfa9898609b9994c679bb4558b9e0997ef892b3
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2023-01-03 14:46:45 +0530

    Create welcome.component.ts

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { LoggingServie } from 'src/app/shared/logging.service';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+  welcomeMessage:string="Welcome to Our Application";
+  data: number=3;
+  title:string="Welcome";
+  name:string="vansh";
+  constructor(private loggingService:LoggingServie){};
+
+
+ 
+  thisFun(){
+    this.loggingService.log("button is called");
+    }
+
+  
+
+}
