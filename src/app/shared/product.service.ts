9de1a9e5b8d9159c653134e60752e951ddef9f91
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2023-01-03 20:28:17 +0530

    Update product.service.ts

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -63,24 +63,20 @@ export class ProductService{
 
 
     createProduct(product:Product):Observable<Product>{
-      //headers variable to set request headers
+      
      const headers= new HttpHeaders({'Content-Type':'application/json'});
  
-         //newProduct spread across product
+    
        const newProduct={...product,id:null};
  
  
-       //return logic starts here
-       //http .post method
-       //generics method -- IProduct
-       //args --3 url , newProduct ,headers
-       //this.url -- declared in the class outside the functions
+       
        return     this.http.post<Product>(this.url,newProduct,{headers})
        .pipe(
          tap(data=>{
  
           console.log('in create new product'+ JSON.stringify(data));
-          //pushing the new data new Product to the products array
+          
           this.products.push(data);
  
          },
@@ -88,12 +84,12 @@ export class ProductService{
          )
        )
    }
-   //delete  api/events --- delete mapping api/events/1
+   
  
    deleteProduct(id:number):Observable<{}>{
      const headers= new HttpHeaders({'Content-Type':'application/json'});
  
-     //@DeleteMapping deleteAll delete url/id  /api/products/111
+   
      const url= `${this.url}/${id}`;
  
      return this.http.delete<Product>(url,{headers})
@@ -101,7 +97,7 @@ export class ProductService{
        tap(data=>{
          console.log('deleted prd'+id);
         const foundIndex = this.products.findIndex(item=>item.id===id);
-        //if product id is not found means index returned will be -1
+       
         if(foundIndex > -1)
         this.products.splice(foundIndex,1);
  
@@ -123,10 +119,10 @@ export class ProductService{
     updateProduct(product:Product):Observable<Product>{
      const headers= new HttpHeaders({'Content-Type':'application/json'});
  
-     //put http method
+    
      const url= `${this.url}/${product.id}`;
  
-     //logic to call http put method to update the product on the given url
+  
      return this.http.put<Product>(url,product, {headers}).pipe(
  
      tap(()=>{console.log('update product'+product.id);
