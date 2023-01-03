dbaa83a45f14fab6658393ca745c344c00122164
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2023-01-03 17:19:13 +0530

    Create employee.component.spec.ts

---
+++ src/app/employee/employee.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { EmployeeComponent } from './employee.component';
+
+describe('EmployeeComponent', () => {
+  let component: EmployeeComponent;
+  let fixture: ComponentFixture<EmployeeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EmployeeComponent ],
+      imports:[FormsModule,ReactiveFormsModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EmployeeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
