fffba411ae974f27c7f9852390d3d7ac9f837273
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2023-01-03 17:03:38 +0530

    Create product-add.component.spec.ts

---
+++ src/app/product/product-add.component.spec.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+
+import { ProductAddComponent } from './product-add.component';
+
+describe('ProductAddComponent', () => {
+  let component: ProductAddComponent;
+  let fixture: ComponentFixture<ProductAddComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductAddComponent ],
+      imports:[FormsModule,ReactiveFormsModule,HttpClientTestingModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProductAddComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+ 
+
+
+
+});
