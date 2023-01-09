5cd6c570b019053be4151fc7f6655391f250f7a3
Shraddhasab <83285507+Shraddhasab@users.noreply.github.com>
2023-01-09 08:58:38 +0530

    Create animal-website.component.ts

---
+++ src/app/animal-website/animal-website.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-website',
+  templateUrl: './animal-website.component.html',
+  styleUrls: ['./animal-website.component.css']
+})
+export class AnimalWebsiteComponent {
+
+}
